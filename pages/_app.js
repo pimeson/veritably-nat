6655865138d7d208bf1936e3523f0f0c38c7f84f
Naohito Sadoshima <nsadoshima@gmail.com>
2021-05-16 00:16:05 -0400

    back btn

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
 import '../styles/globals.css'
 import React from 'react'
 import { GlobalProvider } from '../components/context/_global'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 function MyApp({ Component, pageProps }) {
+  const { pathname } = useRouter()
+  const isHome = pathname === '/'
+
   return <div className="bg-gray-400 bg-opacity-25 min-h-screen flex flex-col">
+    {!isHome && <Link href="/"><button className="self-start font-extrabold p-2">⬅ Take me home</button></Link>}
     <GlobalProvider>
       <Component {...pageProps} />
     </GlobalProvider>
